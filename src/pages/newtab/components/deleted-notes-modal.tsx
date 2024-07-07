91d6e1c3765b875b5f2eeec5bdc3e1c63b285e12
aramrw <106574385+aramrw@users.noreply.github.com>
2024-07-06 20:04:21 -0700

    update deleted notes ui

--- src/pages/newtab/components/deleted-notes-modal.tsx
+++ src/pages/newtab/components/deleted-notes-modal.tsx
@@ -24,11 +24,15 @@ export default function DeletedNotesModal({
   function DeletedNoteItem({ note, index }: { note: NoteType; index: number }) {
     return (
       <ul class="w-full h-fit flex flex-row justify-center items-center gap-2 outline rounded-sm py-0.5 px-2 bg-zinc-900">
-        <li class="w-full text-lg flex flex-row justify-start items-center gap-2">
-          <span class="text-xs">{index}.</span>{" "}
-          <span class="">{note.text}</span>
+        <li class="w-full text-lg flex flex-row justify-between items-center gap-2">
+          <div class="max-w-28 flex flex-row justify-self-start items-center gap-1.5 overflow-x-hidden text-nowrap">
+            <span class="text-xs bg-zinc-800 rounded-md font-medium px-0.5">
+              {index}.
+            </span>
+            <span class="">{note.text}</span>
+          </div>
           <span
-            class="hover:opacity-70 cursor-pointer"
+        class="hover:opacity-70 cursor-pointer bg-zinc-700 rounded-sm pb-0.5"
             onClick={() => deleteNotesForever([note])}
           >
             <TrashIcon />
@@ -39,7 +43,7 @@ export default function DeletedNotesModal({
   }
 
   return (
-    <ul class="min-w-40 w-fit h-full flex flex-col justify-start items-start gap-2">
+    <ul class="min-w-40 w-fit h-full flex flex-col justify-start items-start gap-2 py-1 px-3">
       {deletedNotes().map((note, index) => {
         return <DeletedNoteItem note={note} index={index} />;
       })}

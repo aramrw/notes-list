885edda6811c0954ee8d695e723715c65df1f2ba
aramrw <106574385+aramrw@users.noreply.github.com>
2024-07-06 21:49:10 -0700

    update list-item ui

--- src/pages/newtab/components/list-item.tsx
+++ src/pages/newtab/components/list-item.tsx
@@ -1,7 +1,4 @@
 import "@src/styles/index.css";
-import styles from "./Newtab.module.css";
-import { createSignal, onMount, Accessor } from "solid-js";
-import TrashCan from "@src/assets/img/trashcan.svg";
 import TrashIcon from "./icons/trash";
 import { NoteType } from "../Newtab";
 
@@ -15,11 +12,13 @@ export default function ListItem({
   deleteNotes: (notesToDelete: NoteType[]) => void;
 }) {
   return (
-    <ul class="w-fit flex flex-row justify-self-center items-center gap-2 outline rounded-sm py-0.5 px-2 bg-zinc-900">
+    <ul class="w-fit min-w-60 flex flex-row justify-between items-center gap-2 outline rounded-sm py-0.5 px-2 bg-zinc-900">
       <li>
         <span class="text-xs bg-zinc-800 rounded-md font-medium px-0.5">
           {index}.
-        </span>{" "}
+        </span>
+      </li>
+      <li>
         <span>{note.text}</span>
       </li>
       <li

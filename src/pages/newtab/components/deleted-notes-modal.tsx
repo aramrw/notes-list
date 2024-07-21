842648297c5dc2dff45c9cd68cca4d47fb0b64b4
aramrw <106574385+aramrw@users.noreply.github.com>
2024-07-20 22:12:26 -0700

    deleted-notes-model ui updates

--- src/pages/newtab/components/deleted-notes-modal.tsx
+++ src/pages/newtab/components/deleted-notes-modal.tsx
@@ -23,16 +23,16 @@ export default function DeletedNotesModal({
 }) {
   function DeletedNoteItem({ note, index }: { note: NoteType; index: number }) {
     return (
-      <ul class="w-full h-fit flex flex-row justify-center items-center gap-2 outline rounded-md py-0.5 px-2 bg-zinc-900">
-        <li class="w-full text-lg flex flex-row justify-between items-center gap-2">
-          <div class="max-w-28 flex flex-row justify-self-start items-center gap-1.5 overflow-x-hidden text-nowrap">
-            <span class="text-xs bg-zinc-800 rounded-md font-medium px-0.5">
+      <ul class="w-full h-fit flex flex-row justify-center items-center gap-2 outline outline-zinc-700 rounded-sm py-0.5 px-2 bg-zinc-900">
+        <li class="w-full text-lg flex flex-row justify-between items-center gap-2 overflow-hidden">
+          <div class="max-w-40 flex flex-row justify-self-start items-center gap-1.5 overflow-x-hidden text-nowrap">
+            <span class="text-xs bg-zinc-800 rounded-sm font-medium px-0.5 outline outline-zinc-700 ml-0.5">
               {index}.
             </span>
-            <span class="">{note.text}</span>
+          <span class="text-xl ml-1">{note.text}</span>
           </div>
           <span
-        class="hover:opacity-70 cursor-pointer bg-zinc-700 rounded-sm pb-0.5"
+            class="hover:opacity-70 cursor-pointer bg-zinc-800 rounded-sm outline outline-zinc-700"
             onClick={() => deleteNotesForever([note])}
           >
             <TrashIcon />
@@ -43,7 +43,7 @@ export default function DeletedNotesModal({
   }
 
   return (
-    <ul class="min-w-40 w-fit h-full flex flex-col justify-start items-start gap-2 py-1 px-3">
+    <ul class="min-w-40 w-fit h-full flex flex-col justify-start items-start gap-2 py-1 px-3 max-h-[30rem] overflow-y-scroll">
       {deletedNotes().map((note, index) => {
         return <DeletedNoteItem note={note} index={index} />;
       })}

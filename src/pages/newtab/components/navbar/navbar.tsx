82de55367fde5cda5ce8cd251a91fd02e8512da4
aramrw <106574385+aramrw@users.noreply.github.com>
2024-08-25 23:11:48 -0700

    Create navbar.tsx

---
+++ src/pages/newtab/components/navbar/navbar.tsx
@@ -0,0 +1,61 @@
+import { cn } from "@src/lib/utils";
+import { Accessor, Setter } from "solid-js";
+import { NoteType } from "../../Newtab";
+import copyNotes from "../../utils/copy-notes";
+import ArchiveIcon from "../icons/archive";
+import AddNoteSection from "./add-notes-sections";
+import ImportExportSection from "./import-export-section";
+
+export default function Nav({
+  notes,
+  deletedNotes,
+  setNotes,
+  setDeletedNotes,
+  setIsShowModal,
+}: {
+  notes: Accessor<NoteType[]>;
+  deletedNotes: Accessor<NoteType[]>;
+  setNotes: Setter<NoteType[]>;
+  setDeletedNotes: Setter<NoteType[]>;
+  setIsShowModal: Setter<boolean>;
+}) {
+  return (
+    <nav class="mb-5 mt-0.5 w-full border-b border-b-zinc-700 py-1 pb-1.5 shadow-lg bg-zinc-900 px-3">
+      <menu class="flex flex-row justify-between items-center gap-2 w-full">
+        <li class="w-fit flex h-full flex-col justify-center items-center">
+          <ArchiveIcon
+            class={cn(
+              "hover:opacity-70 cursor-pointer size-8 bg-zinc-900 rounded-sm p-1 border border-zinc-700"
+              // deletedNotes.length === 0 && "hover:opacity-50 cursor-not-allowed opacity-50"
+            )}
+            onClick={() => {
+              if (deletedNotes().length === 0) {
+                return;
+              }
+              setIsShowModal((prev) => !prev);
+            }}
+          />
+        </li>
+        <div class="w-full flex flex-row justify-center items-center gap-2">
+          <li>
+            <AddNoteSection notes={notes} setNotes={setNotes} />
+          </li>
+          <li class="text-base">
+            <button
+              onClick={() => copyNotes(notes(), setNotes, setDeletedNotes)}
+              class="font-medium bg-zinc-900 rounded-sm px-2 text-base hover:opacity-90 border border-zinc-700"
+            >
+              Copy
+            </button>
+          </li>
+          <li class="text-base font-medium opacity-20 select-none">
+            <span>{notes.length}</span> notes
+          </li>
+        </div>
+        <li class="w-fit">
+          <ImportExportSection setNotes={setNotes} />
+        </li>
+      </menu>
+    </nav>
+  );
+}

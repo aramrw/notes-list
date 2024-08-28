df489cfa585b4aded73bf52eae1df8bc6d74e67c
aramrw <106574385+aramrw@users.noreply.github.com>
2024-08-27 18:20:45 -0700

    Update update-manifest.js

--- update-manifest.js
+++ update-manifest.js
@@ -1,9 +1,11 @@
-import * as fs from 'fs';
+import * as fs from "fs";
+
+const FIREFOX_EXTENSION_GECKO_ID = "{bcc01dcc-731c-4e60-bdfa-fc3751c23dfc}";
 
 // Read the JSON file
-fs.readFile('dist/manifest.json', 'utf8', (err, data) => {
+fs.readFile("dist/manifest.json", "utf8", (err, data) => {
   if (err) {
-    console.error('Error reading the file:', err);
+    console.error("Error reading the file:", err);
     return;
   }
 
@@ -12,27 +14,36 @@ fs.readFile('dist/manifest.json', 'utf8', (err, data) => {
   try {
     manifest = JSON.parse(data);
   } catch (e) {
-    console.error('Error parsing JSON:', e);
+    console.error("Error parsing JSON:", e);
     return;
   }
 
-  // Add the scripts property to the background section
+  // Add the scripts property to the background section & remove chrome service worker
   if (manifest.background) {
-    manifest.background.scripts = ['src/pages/background/index.ts'];
+    manifest.background.scripts = ["src/pages/background/index.ts"];
+    delete manifest.background.service_worker;
   } else {
-    manifest.background = { scripts: ['src/pages/background/index.ts'] };
+    manifest.background = { scripts: ["src/pages/background/index.ts"] };
+  }
+
+  // Add the firefox gecko id
+  if (manifest.browser_specific_settings) {
+    manifest.browser_specific_settings.gecko.id = FIREFOX_EXTENSION_GECKO_ID;
+  } else {
+    manifest.browser_specific_settings = {
+      gecko: { id: FIREFOX_EXTENSION_GECKO_ID },
+    };
   }
 
   // Convert the updated JSON back to a string
   const updatedData = JSON.stringify(manifest, null, 2);
 
   // Write the updated JSON back to the file
-  fs.writeFile('dist/manifest.json', updatedData, 'utf8', (err) => {
+  fs.writeFile("dist/manifest.json", updatedData, "utf8", (err) => {
     if (err) {
-      console.error('Error writing the file:', err);
+      console.error("Error writing the file:", err);
       return;
     }
-    console.log('Manifest updated successfully.');
+    console.log("Manifest updated successfully.");
   });
 });
-

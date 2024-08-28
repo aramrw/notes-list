8051337e8587c68cb6cbd16ff7749b0d7d9e032b
aramrw <106574385+aramrw@users.noreply.github.com>
2024-08-27 18:20:35 -0700

    Update manifest.ts

--- src/manifest.ts
+++ src/manifest.ts
@@ -2,7 +2,7 @@ import { defineManifest } from "@crxjs/vite-plugin";
 import packageJson from "../package.json";
 
 // Convert from Semver (example: 0.1.0-beta6)
-const [major, minor, patch, label = "0"] = packageJson.version
+const [majorV, minorV, patchV, labelV = "0"] = packageJson.version
   // can only contain digits, dots, or dash
   .replace(/[^\d.-]+/g, "")
   // split into version parts
@@ -11,7 +11,7 @@ const [major, minor, patch, label = "0"] = packageJson.version
 const manifest = defineManifest(async () => ({
   manifest_version: 3,
   name: packageJson.displayName ?? packageJson.name,
-  version: `${major}.${minor}.${patch}.${label}`,
+  version: `${majorV}.${minorV}.${patchV}.${labelV}`,
   description: packageJson.description,
   options_page: "src/pages/options/index.html",
   background: {

b02c361416ce48028c0a0f7a854dfbf8ff3f8384
aramrw <106574385+aramrw@users.noreply.github.com>
2024-08-27 18:20:43 -0700

    Update export-data.ts

--- src/pages/newtab/utils/export-data.ts
+++ src/pages/newtab/utils/export-data.ts
@@ -1,4 +1,11 @@
 import { NoteType } from "../Newtab";
+import packageJson from "../../../../package.json";
+
+export const [majorV, minorV, patchV, labelV = "0"] = packageJson.version
+  // can only contain digits, dots, or dash
+  .replace(/[^\d.-]+/g, "")
+  // split into version parts
+  .split(/[.-]/);
 
 export function exportData() {
   return new Promise((resolve, reject) => {
@@ -6,25 +13,33 @@ export function exportData() {
       if (chrome.runtime.lastError) {
         return reject(new Error(chrome.runtime.lastError.message));
       }
+      chrome.storage.local.get("deleted", (_dRes) => {
+        if (chrome.runtime.lastError) {
+          return reject(new Error(chrome.runtime.lastError.message));
+        }
+        const notes: NoteType[] = res.notes || [];
+        //const dNotes: NoteType[] = dRes.deleted || [];
 
-      const notes: NoteType[] = res.notes;
+        // Convert notes to a JSON string
+        const notesJson = JSON.stringify(notes, null, 2);
+        //const dNotesJson = JSON.stringify(notes, null, 2);
 
-      // Convert notes to a JSON string
-      const notesJson = JSON.stringify(notes, null, 2);
+        // Create a Blob from the JSON string
+        const blob = new Blob([notesJson], { type: "application/json" });
+        //const dBlob = new Blob([dNotesJson], { type: "application/json" });
 
-      // Create a Blob from the JSON string
-      const blob = new Blob([notesJson], { type: "application/json" });
+        // Trigger the download directly
+        const url = URL.createObjectURL(blob);
+        const downloadLink = document.createElement("a");
+        downloadLink.style.opacity = "1";
+        downloadLink.href = url;
+        downloadLink.download = `qnt_backup_${majorV}.${minorV}.${patchV}.${labelV}`;
+        downloadLink.click();
 
-      // Trigger the download directly
-      const url = URL.createObjectURL(blob);
-      const downloadLink = document.createElement("a");
-      downloadLink.href = url;
-      downloadLink.download = "notes.json";
-      downloadLink.click();
-
-      // Clean up the URL object
-      URL.revokeObjectURL(url);
-      resolve(notes);
+        // Clean up the URL object
+        URL.revokeObjectURL(url);
+        resolve(notes);
+      });
     });
   });
 }

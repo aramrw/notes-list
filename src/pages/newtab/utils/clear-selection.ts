304d205e976c5b1d57862b7c0caa2a6ca9db2b0e
aramrw <106574385+aramrw@users.noreply.github.com>
2024-08-28 19:33:59 -0700

    Update clear-selection.ts

--- src/pages/newtab/utils/clear-selection.ts
+++ src/pages/newtab/utils/clear-selection.ts
@@ -1,9 +1,23 @@
+import { detectBrowser } from "@src/lib/detect-browser";
+
 export default function clearSelection() {
   if (window.getSelection) {
+    let browser = detectBrowser().name;
     const selection = window.getSelection();
-    if (selection) {
-      selection.removeAllRanges();
-    } else {
-		}
+
+    if (browser === 'Chrome' || browser === 'Chromium') {
+      if (selection) {
+        selection.removeAllRanges();
+      }
+    } else if (browser === 'Firefox') {
+      const activeElement = document.activeElement as HTMLInputElement | HTMLTextAreaElement;
+      if (activeElement && typeof activeElement.selectionStart === 'number') {
+        activeElement.setSelectionRange(activeElement.selectionStart, activeElement.selectionStart);
+      }
+      if (selection) {
+        selection.removeAllRanges(); // This ensures that the selection is cleared in Firefox
+      }
+    }
   }
 }
+
